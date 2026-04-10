3343c6485c2334411a4c947d74fde70637f866be
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2026-04-10 15:38:30 +0200

    Add 05_watch_web_app_db

---
+++ 05_watch_web_app_db/main.py
@@ -0,0 +1,48 @@
+import os
+from datetime import datetime, timezone
+
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Request(Base):
+    __tablename__ = "request"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def get_root(db: Session = Depends(get_db)):
+    db.add(Request())
+    db.commit()
+    count = db.scalar(func.count(Request.id))
+    return {"request_count": count}
